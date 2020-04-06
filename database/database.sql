81637e59d7f972b1e08a5cb8a92f9a19f57432e5
lilianalmeida <up201706908@fe.up.pt>
2020-04-06 22:06:51 +0100

    Updated database scripts.
    Co-authored-by: up201706791 <up201706791@fe.up.pt>
    Co-authored-by: up201706892 <up201706892@fe.up.pt>
    Co-authored-by: up201703782 <up201703782@fe.up.pt>

--- database/database.sql
+++ database/database.sql
@@ -2,8 +2,8 @@
 -- Drop old schmema
 -----------------------------------------
 
-DROP TABLE IF EXISTS "user" CASCADE;
-DROP TABLE IF EXISTS "admin" CASCADE;
+DROP TABLE IF EXISTS member_user CASCADE;
+DROP TABLE IF EXISTS admin_user CASCADE;
 DROP TABLE IF EXISTS community CASCADE;
 DROP TABLE IF EXISTS post CASCADE;
 DROP TABLE IF EXISTS comment CASCADE;
@@ -36,6 +36,9 @@ DROP FUNCTION IF EXISTS post_not_member_community() CASCADE;
 DROP FUNCTION IF EXISTS comment_not_member_community() CASCADE;
 DROP FUNCTION IF EXISTS comment_vote_not_member_community() CASCADE;
 DROP FUNCTION IF EXISTS post_vote_not_member_community() CASCADE;
+DROP FUNCTION IF EXISTS change_community_privacy() CASCADE;
+DROP FUNCTION IF EXISTS verify_pk_follow_request() CASCADE;
+DROP FUNCTION IF EXISTS verify_pk_join_community_request() CASCADE;
 
 DROP TRIGGER IF EXISTS block_user ON block_user;
 DROP TRIGGER IF EXISTS vote_on_comment ON comment_vote;
@@ -46,6 +49,10 @@ DROP TRIGGER IF EXISTS post_not_member_community ON post;
 DROP TRIGGER IF EXISTS comment_not_member_community ON comment;
 DROP TRIGGER IF EXISTS comment_vote_not_member_community ON comment_vote;
 DROP TRIGGER IF EXISTS post_vote_not_member_community ON post_vote;
+DROP TRIGGER IF EXISTS change_community_privacy ON member_user;
+DROP TRIGGER IF EXISTS verify_pk_follow_request ON follow_resquest;
+DROP TRIGGER IF EXISTS verify_pk_join_community_request ON join_community_resquest;
+
 
 -----------------------------------------
 -- Types
@@ -59,7 +66,7 @@ CREATE TYPE status_types AS ENUM ('accepted', 'denied', 'pending');
 -- Tables
 -----------------------------------------
 
-CREATE TABLE "user" (
+CREATE TABLE member_user (
     id SERIAL PRIMARY KEY,
     username text UNIQUE NOT NULL,
     first_name text NOT NULL,
@@ -73,7 +80,7 @@ CREATE TABLE "user" (
     credibility int DEFAULT 0
 );
 
-CREATE TABLE "admin" (
+CREATE TABLE admin_user (
     id SERIAL PRIMARY KEY,
     username text UNIQUE NOT NULL,
     first_name text NOT NULL,
@@ -87,7 +94,7 @@ CREATE TABLE community (
     name text UNIQUE NOT NULL,
     image text,
     private boolean NOT NULL,
-    id_owner int REFERENCES "user"
+    id_owner int REFERENCES member_user ON DELETE SET NULL
 );
 
 CREATE TABLE post (
@@ -95,26 +102,26 @@ CREATE TABLE post (
     image text,
     title text NOT NULL,
     content text NOT NULL,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL, 
+    time_stamp TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL, 
     upvotes int NOT NULL DEFAULT 0,
     downvotes int NOT NULL DEFAULT 0,
-    id_author int REFERENCES "user",
+    id_author int REFERENCES member_user ON DELETE SET NULL,
     id_community int NOT NULL REFERENCES community ON DELETE CASCADE
 );
 
 CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
     content text NOT NULL,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    time_stamp TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     upvotes int NOT NULL DEFAULT 0,
     downvotes int NOT NULL DEFAULT 0,
-    id_author int REFERENCES "user",
+    id_author int REFERENCES member_user ON DELETE SET NULL,
     id_post int NOT NULL REFERENCES post ON DELETE CASCADE
 );
 
 CREATE TABLE post_vote (
     vote_type vote_types NOT NULL,
-    id_user int REFERENCES "user" ON DELETE CASCADE,
+    id_user int REFERENCES member_user ON DELETE CASCADE,
     id_post int REFERENCES post ON DELETE CASCADE,
     PRIMARY KEY (id_user, id_post)
 );
@@ -122,9 +129,9 @@ CREATE TABLE post_vote (
 CREATE TABLE report (
     id SERIAL PRIMARY KEY,
     reason text NOT NULL,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-    id_admin int NOT NULL REFERENCES "admin",
-    id_user int NOT NULL REFERENCES "user"
+    time_stamp TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    id_admin int NOT NULL REFERENCES admin_user,
+    id_user int NOT NULL REFERENCES member_user ON DELETE SET NULL
 );
 
 CREATE TABLE comment_report (
@@ -144,7 +151,7 @@ CREATE TABLE community_report (
 
 CREATE TABLE user_report (
     id_report int PRIMARY KEY REFERENCES report ON DELETE CASCADE,
-    id_user int NOT NULL REFERENCES "user" ON DELETE CASCADE
+    id_user int NOT NULL REFERENCES member_user ON DELETE CASCADE
 );
 
 CREATE TABLE reply (
@@ -154,17 +161,17 @@ CREATE TABLE reply (
 
 CREATE TABLE comment_vote (
     vote_type vote_types NOT NULL,
-    id_user int REFERENCES "user" ON DELETE CASCADE,
+    id_user int REFERENCES member_user ON DELETE CASCADE,
     id_comment int REFERENCES comment ON DELETE CASCADE,
     PRIMARY KEY (id_user, id_comment)
 );
 
 CREATE TABLE request (
     id SERIAL PRIMARY KEY,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    time_stamp TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     status status_types NOT NULL DEFAULT 'pending',
-    id_receiver int NOT NULL REFERENCES "user" ON DELETE CASCADE,
-    id_sender int NOT NULL REFERENCES "user" ON DELETE CASCADE
+    id_receiver int NOT NULL REFERENCES member_user ON DELETE CASCADE,
+    id_sender int NOT NULL REFERENCES member_user ON DELETE CASCADE
 );
 
 CREATE TABLE follow_request (
@@ -178,24 +185,24 @@ CREATE TABLE join_community_request (
 
 CREATE TABLE notification (
     id SERIAL PRIMARY KEY,
-    "read" boolean NOT NULL DEFAULT FALSE,
+    is_read boolean NOT NULL DEFAULT FALSE,
     id_request int UNIQUE NOT NULL REFERENCES request ON DELETE CASCADE
 );
 
 CREATE TABLE block_user (
-    blocked_user int REFERENCES "user" ON DELETE CASCADE,
-    blocker_user int REFERENCES "user" ON DELETE CASCADE,
+    blocked_user int REFERENCES member_user ON DELETE CASCADE,
+    blocker_user int REFERENCES member_user ON DELETE CASCADE,
     PRIMARY KEY (blocked_user, blocker_user)
 );
 
 CREATE TABLE follow_user (
-    id_follower int REFERENCES "user" ON DELETE CASCADE,
-    id_followed int REFERENCES "user" ON DELETE CASCADE,
+    id_follower int REFERENCES member_user ON DELETE CASCADE,
+    id_followed int REFERENCES member_user ON DELETE CASCADE,
     PRIMARY KEY (id_follower, id_followed)
 );
 
 CREATE TABLE community_member (
-    id_user int REFERENCES "user" ON DELETE CASCADE,
+    id_user int REFERENCES member_user ON DELETE CASCADE,
     id_community int REFERENCES community ON DELETE CASCADE,
     PRIMARY KEY (id_user, id_community)
 );
@@ -271,13 +278,13 @@ BEGIN
 			WHERE id = NEW.id_comment;
     END IF;
 	
-	UPDATE "user" 
+	UPDATE member_user 
 		SET credibility = credibility + sqrt(abs(subquery.upvotes - subquery.downvotes)) * sign(subquery.upvotes - subquery.downvotes) 
 		FROM 
 			(SELECT comment.id_author AS author, comment.upvotes AS upvotes, comment.downvotes AS downvotes
 				FROM comment
 				WHERE comment.id = NEW.id_comment) AS subquery 
-		WHERE "user".id = subquery.author;
+		WHERE member_user.id = subquery.author;
 	
     RETURN NEW;
 END
@@ -328,13 +335,13 @@ BEGIN
 			WHERE id = NEW.id_post;
     END IF;
 	
-	UPDATE "user" 
+	UPDATE member_user 
 		SET credibility = credibility + sqrt(abs(subquery.upvotes - subquery.downvotes)) * sign(subquery.upvotes - subquery.downvotes) 
 		FROM (
 			SELECT post.id AS post_id, post.id_author AS author, post.upvotes AS upvotes, post.downvotes AS downvotes
 				FROM post
 				WHERE post.id = NEW.id_post) AS subquery 
-		WHERE "user".id = subquery.author;
+		WHERE member_user.id = subquery.author;
 	
     RETURN NEW;
 END
@@ -350,7 +357,7 @@ CREATE TRIGGER vote_on_post
 CREATE FUNCTION create_notification() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-    INSERT INTO notification ("read", id_request) VALUES (FALSE, NEW.id);
+    INSERT INTO notification (is_read, id_request) VALUES (FALSE, NEW.id);
 
     RETURN NULL;
 END
@@ -368,7 +375,7 @@ BEGIN
     IF EXISTS (
 		SELECT *
 			FROM post
-			WHERE NEW.id_post = id AND NEW."date" < "date")
+			WHERE NEW.id_post = id AND NEW.time_stamp < time_stamp)
 		THEN
 			RAISE EXCEPTION 'The comment''s date must be after the post''s date. %', New.id_post ;
 	END IF;
@@ -387,12 +394,18 @@ CREATE TRIGGER check_comment_date
 CREATE FUNCTION post_not_member_community() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-    IF EXISTS 
-		(SELECT * FROM "user", community
-	 	 WHERE NEW.id_community = community.id AND community.private = TRUE AND NEW.id_author = "user".id AND 
-		 NOT EXISTS(SELECT * FROM community_member CM WHERE CM.id_user = NEW.id_author AND CM.id_community = NEW.id_community)) THEN
-	   RAISE EXCEPTION 'The user cannot post on this community because you''re not a member % % %', NEW.id, NEW.id_community, NEW.id_author;
+    IF EXISTS (
+		SELECT *
+			FROM member_user, community
+			WHERE NEW.id_community = community.id AND community.private = TRUE AND NEW.id_author = member_user.id AND 
+			NOT EXISTS (
+				SELECT *
+					FROM community_member CM
+					WHERE CM.id_user = NEW.id_author AND CM.id_community = NEW.id_community))
+				THEN
+					RAISE EXCEPTION 'The user cannot post in/on this community % % %', NEW.id, NEW.id_community, NEW.id_author;
 	END IF;
+
 	RETURN NEW;
 END
 $BODY$
@@ -408,8 +421,8 @@ CREATE FUNCTION comment_not_member_community() RETURNS TRIGGER AS
 $BODY$
 BEGIN
     IF EXISTS 
-		(SELECT * FROM "user", community, post
-	 	 WHERE NEW.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_author = "user".id AND 
+		(SELECT * FROM member_user, community, post
+	 	 WHERE NEW.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_author = member_user.id AND 
 		 NOT EXISTS(SELECT * FROM community_member CM WHERE CM.id_user = NEW.id_author AND CM.id_community = community.id)) THEN
 	   RAISE EXCEPTION 'The user cannot comment on this post because you''re not member of the community % % %', NEW.id, NEW.id_post, NEW.id_author;
 	END IF;
@@ -428,8 +441,8 @@ CREATE FUNCTION comment_vote_not_member_community() RETURNS TRIGGER AS
 $BODY$
 BEGIN
     IF EXISTS 
-		(SELECT * FROM "user", community, post, comment
-	 	 WHERE NEW.id_comment = comment.id AND comment.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_user = "user".id AND 
+		(SELECT * FROM member_user, community, post, comment
+	 	 WHERE NEW.id_comment = comment.id AND comment.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_user = member_user.id AND 
 		 NOT EXISTS(SELECT * FROM community_member CM WHERE CM.id_user = NEW.id_user AND CM.id_community = community.id)) THEN
 	   RAISE EXCEPTION 'The user cannot vote on this comment because you''re not member of the community % %', NEW.id_user, NEW.id_comment;
 	END IF;
@@ -449,8 +462,8 @@ $BODY$
 BEGIN
     IF EXISTS (
 		SELECT *
-		FROM "user", community, post
-	 	 	WHERE NEW.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_user = "user".id AND 
+		FROM member_user, community, post
+	 	 	WHERE NEW.id_post = post.id AND post.id_community = community.id AND community.private = TRUE AND NEW.id_user = member_user.id AND 
 		 	NOT EXISTS (
 				SELECT *
 					FROM community_member CM
@@ -468,6 +481,64 @@ CREATE TRIGGER post_vote_not_member_community
     AFTER INSERT ON post_vote
     FOR EACH ROW
     EXECUTE PROCEDURE post_vote_not_member_community();
+
+
+CREATE FUNCTION verify_pk_follow_request() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF EXISTS (
+		SELECT *
+			FROM join_community_request
+			WHERE id = NEW.id)
+		THEN
+			RAISE EXCEPTION 'A follow request cannot be associated with the same request as a join community request.';
+	END IF;
+	RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+ 
+CREATE TRIGGER verify_pk_follow_request
+    AFTER INSERT ON follow_request
+    FOR EACH ROW
+    EXECUTE PROCEDURE verify_pk_follow_request();
+	
+
+CREATE FUNCTION verify_pk_join_community_request() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF EXISTS (
+		SELECT *
+			FROM follow_request
+			WHERE id = NEW.id)
+		THEN 
+			RAISE EXCEPTION 'A join community request cannot be associated with the same request as a follow request.';
+	END IF;
+	RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+ 
+CREATE TRIGGER verify_pk_join_community_request
+    AFTER INSERT ON join_community_request
+    FOR EACH ROW
+    EXECUTE PROCEDURE verify_pk_join_community_request();
+	
+CREATE FUNCTION change_community_privacy() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    UPDATE community
+		SET private = FALSE
+		WHERE id_owner = OLD.id AND private = TRUE;
+	RETURN OLD;
+END
+$BODY$
+LANGUAGE plpgsql;
+ 
+CREATE TRIGGER change_community_privacy
+    BEFORE DELETE ON member_user
+    FOR EACH ROW
+    EXECUTE PROCEDURE change_community_privacy();
 
 -----------------------------------------
 -- end
