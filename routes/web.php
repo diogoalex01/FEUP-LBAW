17ffb09f05cbde601cfd5b485a41eb3dfb382622
joaohenriqueluz <joaohenriqueluz99@gmail.com>
2020-04-21 01:44:57 +0100

    Added .zip folder. Removed non-essential LBAW template code.

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,7 @@ Route::get('/about', 'PageController@about')->name('about');
 // API
 Route::post('api/communities', 'CommunityController@get_all');
 
-//Route::get('/home', 'Auth\LoginController@home');
-// Cardss
-// Route::get('cards', 'CardController@list');
-// Route::get('cards/{id}', 'CardController@show');
-
-// API
-// Route::put('api/cards', 'CardController@create');
-// Route::delete('api/cards/{card_id}', 'CardController@delete');
-// Route::put('api/cards/{card_id}/', 'ItemController@create');
-// Route::post('api/item/{id}', 'ItemController@update');
-// Route::delete('api/item/{id}', 'ItemController@delete');
-
 // Authentication
-// Route::get('login', 'Auth\LoginController@showLoginForm')
 Route::post('login', 'Auth\LoginController@login')->name('login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-// Route::get('register', 'Auth\RegisterController@showRegistrationForm')
 Route::post('register', 'Auth\RegisterController@register')->name('register');
